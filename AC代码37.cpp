cd7ce149db53ab524138fdb983d4f97a1491fd45
qyy-0712 <330429668@qq.com>
2025-12-12 10:41:45 +0800

    Add files via upload

---
+++ AC代码37.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getheight(TreeNode* cur){
+        if(cur==nullptr)return 0;
+        int lefttree=getheight(cur->left);
+        if(lefttree==-1)return -1;
+        int righttree=getheight(cur->right);
+        if(righttree==-1)return -1;
+        if(abs(righttree-lefttree)>1)return -1;
+        return 1+max(lefttree,righttree);
+    }
+    bool isBalanced(TreeNode* root) {
+        if(getheight(root)==-1)return false;
+        else return true;
+    }
+};
